4198b2c8df5ed3e31a2fc6cc62baf7398cac33cf
josbernikon <47560133+josbernikon@users.noreply.github.com>
2020-02-13 13:37:24 +0100

    Add files via upload

---
+++ WebXports/src/main/java/es/urjc/etsii/dadWebXports/PlantillaRepository.java
@@ -0,0 +1,7 @@
+package es.urjc.etsii.dadWebXports;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlantillaRepository extends JpaRepository<Plantilla, Long> {
+
+}
